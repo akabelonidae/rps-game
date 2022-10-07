ee6b9bb40fcbaca9d046483a64b870aa9ee837c0
Oguz <aka@pop-os.localdomain>
2022-10-07 08:40:47 +0300

    changed the style a little. not yet optimized.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // variables for cleaner code
-const playerSelectRock = document.querySelector('.rock-img');
-const playerSelectPaper = document.querySelector('.paper-img');
-const playerSelectScissors = document.querySelector('.scissors-img');
+const playerSelectionRock = document.querySelector('.rock-img');
+const playerSelectionPaper = document.querySelector('.paper-img');
+const playerSelectionScissors = document.querySelector('.scissors-img');
 const playerFrameImg = document.getElementById('player-img');
 const computerFrameImg = document.getElementById('computer-img');
 
@@ -12,38 +12,38 @@ const middleText = document.querySelector('.middle-text');
 // Score variables
 let playerScore = 0;
 let computerScore = 0;
-let playerSelect;
+let playerSelection;
 
 // Functions for selecting rock paper and scissors
 const rockSelected = function () {
-  playerSelect = 1;
+  playerSelection = 1;
   playerFrameImg.src = 'img/rock.png';
   playerFrameImg.style.height = '150px';
-  computersTurn(playerSelect);
+  computersTurn(playerSelection);
 };
 
 const paperSelected = function () {
-  playerSelect = 2;
+  playerSelection = 2;
   playerFrameImg.src = 'img/paper.png';
   playerFrameImg.style.height = '150px';
-  computersTurn(playerSelect);
+  computersTurn(playerSelection);
 };
 
 const scissorsSelected = function () {
-  playerSelect = 3;
+  playerSelection = 3;
 
   playerFrameImg.src = 'img/scissors.png';
   playerFrameImg.style.height = '150px';
-  computersTurn(playerSelect);
+  computersTurn(playerSelection);
 };
 
 // AEL for rock paper scissors
-playerSelectPaper.addEventListener('click', paperSelected);
-playerSelectRock.addEventListener('click', rockSelected);
-playerSelectScissors.addEventListener('click', scissorsSelected);
+playerSelectionPaper.addEventListener('click', paperSelected);
+playerSelectionRock.addEventListener('click', rockSelected);
+playerSelectionScissors.addEventListener('click', scissorsSelected);
 
 // Function for computer's turn (definitely not the cleanest solution)
-const computersTurn = function (playerSelect) {
+const computersTurn = function (playerSelection) {
   // Computer's turn
   const computerSelect = Math.trunc(Math.random() * 3) + 1;
   if (computerSelect === 1) {
@@ -57,37 +57,35 @@ const computersTurn = function (playerSelect) {
     computerFrameImg.style.height = '150px';
   }
   //   Win lose implementation
-  if (playerSelect === computerSelect) {
-    console.log('draw');
+  if (playerSelection === computerSelect) {
     middleText.textContent = 'Draw!';
   } else if (
-    (playerSelect === 1 && computerSelect === 2) ||
-    (playerSelect === 2 && computerSelect === 3) ||
-    (playerSelect === 3 && computerSelect === 1)
+    (playerSelection === 1 && computerSelect === 2) ||
+    (playerSelection === 2 && computerSelect === 3) ||
+    (playerSelection === 3 && computerSelect === 1)
   ) {
-    console.log('lost');
     computerScore = computerScore + 1;
     computerScoreText.textContent = `Computer Score: ${computerScore}`;
     middleText.textContent = 'You lost!';
   } else {
-    console.log('win');
     playerScore = playerScore + 1;
     playerScoreText.textContent = `Player Score: ${playerScore}`;
     middleText.textContent = 'You won!';
   }
 
-  //   Implementation of win condition
+  //   Implementation of win/lose
+  const winLostMessage = function (message, backgroundColor) {
+    document.querySelector('#main-section').style.display = 'none';
+    document.querySelector('.img-selection').style.display = 'none';
+    document.querySelector('.middle-text').style.display = 'none';
+
+    document.querySelector('#title').textContent = message;
+    document.querySelector('body').style.backgroundColor = backgroundColor;
+  };
+
   if (playerScore === 3) {
-    document.querySelector('body').style.backgroundColor = 'green';
-    document.querySelector('#title').textContent = 'You won!';
-    document.querySelector('.main-section').style.display = 'none';
-    document.querySelector('.img-selection').style.display = 'none';
-    document.querySelector('.middle-text').style.display = 'none';
+    winLostMessage('You won!', 'green');
   } else if (computerScore === 3) {
-    document.querySelector('body').style.backgroundColor = 'red';
-    document.querySelector('#title').textContent = 'You lost!';
-    document.querySelector('.main-section').style.display = 'none';
-    document.querySelector('.img-selection').style.display = 'none';
-    document.querySelector('.middle-text').style.display = 'none';
+    winLostMessage('You lost!', 'red');
   }
 };
